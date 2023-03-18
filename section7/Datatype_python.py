4bce9d78ad4bac1e09a383c86b5f636ede02c712
Mr.Eldar <elka.bekir@gmail.com>
2023-03-17 21:52:10 -0400

    learning string

---
+++ section7/Datatype_python.py
@@ -0,0 +1,18 @@
+print(True)
+print(4934)
+
+game_over = True
+print(game_over)
+game_over = False
+print(game_over)
+
+some_string = "11"
+print(some_string)
+
+type(some_string)
+
+type(11)
+
+some_string = "hello i am 7 years old"
+
+print(some_string)
